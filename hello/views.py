6a24c322b56933628ddab425d5b185116b449b01
AliToori <alituri512@gmail.com>
2022-04-21 22:44:55 +0500

    Updated Environment

--- hello/views.py
+++ hello/views.py
@@ -4,12 +4,10 @@ from pathlib import Path
 
 import pandas as pd
 from django.shortcuts import render
-from dotenv import load_dotenv
 from pybit import usdt_perpetual
 
 from .models import Greeting
 
-load_dotenv()
 
 logging.config.dictConfig({
     "version": 1,
@@ -60,17 +58,14 @@ ws = usdt_perpetual.WebSocket(test=False, api_key=api_key, api_secret=api_secret
 
 
 # Check on your order and position through WebSocket.
-def handle_orderbook(message):
+def handle_position(message):
     df = pd.DataFrame([message])
     df = df.loc[:, ['E', 's', 'c']]
     df.columns = ['Time', 'Symbol', 'Price']
     df["Price"] = df["Price"].astype(float)
     df["Time"] = pd.to_datetime(df["Time"], unit='ms')
-    file_path = str(self.PROJECT_ROOT / f'{df.iloc[0]["Symbol"]}.csv')
+    file_path = str(PROJECT_ROOT / f'{symbol}.csv')
     df.to_csv(file_path, index=False)
-
-def handle_position(message):
-    print(message)
 
 
 # Create your views here.
@@ -79,37 +74,35 @@ def index(request):
 
 
 def trades(request):
-    # Get orderbook.
-    # account_balance = session.get_wallet_balance()
-    # print(f"Account Balance: {account_balance}")
+
     # {{plot_7}} {{plot_8}} {{plot_4}}
     # buyprice takeprofit stoploss
     if request.method == 'POST':
+        account_balance = session.get_wallet_balance()
+        LOGGER.info(f"Account Balance: {account_balance}")
         order_data = {
             "Symbol": request.POST["symbol"],
             "BuyPrice": request.POST["buyprice"],
             "TakeProfit": request.POST["takeprofit"],
             "StopLoss": request.POST["stoploss"]
         }
-        print(session.place_conditional_order(
+        quantity = round(account_balance / order_data["BuyPrice"])
+        buy_order = session.place_conditional_order(
             symbol=symbol,
             order_type="Limit",
             side="Buy",
-            qty=1,
-            price=8100,
-            base_price=16100,
-            stop_px=8150,
+            qty=quantity,
+            price=order_data["BuyPrice"],
+            base_price=order_data["BuyPrice"],
+            stop_px=order_data["BuyPrice"],
             time_in_force="GoodTillCancel",
             order_link_id="cus_order_id_1",
             reduce_only=False,
-            close_on_trigger=False
-        ))
+            close_on_trigger=False,
+        )
+        LOGGER.info(f"Buy Order has been placed: {buy_order}")
         # Subscribe to the execution topic
         ws.position_stream(handle_position)
-        while True:
-            data = ws.fetch(subs[0])
-            if data:
-                print(data)
         # return render(request, 'trades.html', {"order_data": order_data})
     order_data = {
         "symbol": "LUNAUSDT",
