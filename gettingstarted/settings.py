6db082ef5638242b581d5c37265ef496331394a1
paulina7m <paulina7m@gmail.com>
2022-04-25 14:10:20 +0300

    updated local envs

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be used, if set, instead)."
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_VALUE')
 
 ALLOWED_HOSTS = []
 
