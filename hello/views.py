70828de1a3965e951aa16d125fb2bc21a9256edf
AliToori <alituri512@gmail.com>
2022-04-26 05:49:19 +0500

    Resolved TradingView Alert Issue

--- hello/views.py
+++ hello/views.py
@@ -11,7 +11,7 @@ from pybit import usdt_perpetual
 from django.views.decorators.csrf import csrf_exempt
 from .models import Greeting
 
-load_dotenv()
+# load_dotenv()
 
 logging.config.dictConfig({
     "version": 1,
